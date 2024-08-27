c89fe0eead4b6d9431001f33a69718f12e1bfa65
Flori O <floroldani@hotmail.com>
2024-08-27 12:28:59 -0300

    sacar el efecto parallax en mobile

--- src/components/home/Portadas.tsx
+++ src/components/home/Portadas.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useEffect, useState } from "react";
 import { Montserrat } from "next/font/google";
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, useBreakpointValue  } from "@chakra-ui/react";
 
 export const titleFont = Montserrat({ 
   subsets: ["latin"], 
@@ -23,11 +23,21 @@ const Portadas = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  const backgroundPositionY = useBreakpointValue({
+    base: `${0}px`,
+    sm: `${0}px`,
+    md: `${scrollY * 0.7}px`,
+    lg: `${scrollY * 0.7}px`,
+    xl: `${scrollY * 0.7}px`,
+  });
   
   return (
     <Flex position='relative' w='100%' h='100vh' direction='column' overflow='hidden' alignItems='center' justifyContent='center' id="portada-section">
 
-      <Flex position='absolute' w='100%' h='100%' bgSize='cover' bgPos='center' bgAttachment='fixed' bgRepeat='no-repeat' transform='translateZ(0)' style={{ backgroundImage: "url('/portada.jpg')", backgroundPositionY: `${scrollY * 0.7}px` }}/>
+      <Flex position='absolute' w='100%' h='100%' bgSize='cover' bgPos='center' bgAttachment='fixed' bgRepeat='no-repeat' transform='translateZ(0)' 
+      style={{ backgroundImage: "url('/portada.jpg')", backgroundPositionY: backgroundPositionY}}
+      />
 
       <Flex w='100%' position='relative' direction='column' alignItems='center' justifyContent='center' color='white' gap={4}>
         <Text className={titleFont.className} fontWeight={400} fontSize={{base:'38px', sm:'100px'}} color='white' variant='h2'>BIENVENIDO</Text>
